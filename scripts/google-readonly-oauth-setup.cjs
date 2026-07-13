5063eb77c321d7d342d35c8928c96f6ffbcb1bf1
TWooden2-cyber <12345678+TWooden2-cyber@users.noreply.github.com>
2026-07-12 20:19:53 -0400

    Use Gmail metadata for Google connection checks

--- scripts/google-readonly-oauth-setup.cjs
+++ scripts/google-readonly-oauth-setup.cjs
@@ -8,7 +8,7 @@ const { google } = require("googleapis");
 const SCOPES = [
   "https://www.googleapis.com/auth/drive.metadata.readonly",
   "https://www.googleapis.com/auth/calendar.readonly",
-  "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/gmail.metadata",
   "https://www.googleapis.com/auth/tasks.readonly"
 ];
 
